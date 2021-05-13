83934b8a05ed305983c89b12eb207df8a3d6aa92
KM-drago <62215420+KM-drago@users.noreply.github.com>
2021-05-13 20:20:30 +0530

    Assignment 1 V1

---
+++ Assignment 1/volume.scala
@@ -0,0 +1,5 @@
+object volume extends App {
+  def calculateVolume(x:Double)=(4.0/3.0)*math.Pi*math.pow(x,3)
+
+  println(calculateVolume(5))
+}
---
+++ Assignment 1/wholesalecost.scala
@@ -0,0 +1,6 @@
+object wholesalecost extends App{
+  def calculateCost(x:Int):Double=24.95*.6*x+(if(x>50) 50*3+(x-50)*.75 else x*3)
+
+  println(calculateCost(60))
+  println(calculateCost(51))
+}
